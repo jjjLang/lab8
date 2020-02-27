85ee11bbc31218845315d56dfd11e8c46b7ea80f
Jing Lang <georgina_lang@yahoo.com>
2020-02-27 10:44:17 -0800

    thirdpartyAPI lab

---
+++ public/js/introHCI.js
@@ -0,0 +1,51 @@
+// 'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Page ready");
+ 	initCamera();
+ 	initMap();
+ 	initGestures();
+ 	initRSVPForm();
+}
+
+// init jQuery gestures  
+function initGestures() {
+	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold",tapholdHandler);
+
+		function tapholdHandler(event){
+			//get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: "+targetIDPrefix);
+			//show info
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
+}
+
+// init RSVP form submit listener
+function initRSVPForm() {
+  $('#rsvpForm').submit(function(e) {
+
+  	//Prevent default submit + reload (we only want thte submit part)
+  	e.preventDefault();
+  	console.log("submitting form...")
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	//send the POST request
+  	$.post('addRSVP',{ rsvpEmail: rsvpEmail},postCallback);
+  });
+
+  function postCallback(res) {
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val(''); // Clear form
+  }
+}
---
+++ routes/rsvp.js
@@ -0,0 +1,21 @@
+var data = {
+  rsvp: ['ixd@ucsd.edu']
+};
+
+/*
+ * GET tasks page.
+ */
+exports.adminView = function(request, response){
+  response.render('rsvp', data);
+};
+
+exports.addRSVP = function(request, response) {
+	var rsvpEmail = request.body.rsvpEmail;
+	//This will print to your terminal whenever the POST request is made
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail); // Add to current data
+
+	//Let us send back some data so that browser knows it worked
+	response.send(rsvpEmail);
+};
